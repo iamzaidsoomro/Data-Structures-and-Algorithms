6a7b46dab505fa23dda8f3dbedac1e159bea15f3
iamzaidsoomro <zaid3614@gmail.com>
2022-01-18 23:49:22 +0500

    Circular LinkedList initiated

---
+++ Linked_List/app.java
@@ -0,0 +1,34 @@
+package Linked_List;
+
+public class app {
+    public static void main(String args[]) {
+
+    }
+}
+
+class CircularLinkedList {
+
+    private class Node {
+        private int data;
+        private Node next;
+        private int index;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private int length, limit;
+
+    public CircularLinkedList() {
+        length = 0;
+        limit = -1;
+    }
+
+    public CircularLinkedList(int limit) {
+        if (limit >= 0) {
+            this.limit = limit;
+            this.length = 0;
+        }
+    }
+}
